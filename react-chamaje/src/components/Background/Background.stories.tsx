552465323862f0814bf8dd7560a01c06cba7228a
n1kito <mjallada@student.42.fr>
2023-07-02 18:54:24 +0200

    works but needs some work if we want to use it, I prefer the colors on the one Jee made

---
+++ react-chamaje/src/components/Background/Background.stories.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import Background from './Background';
+
+export default {
+	title: 'Background',
+};
+
+export const Default = () => <Background />;
+
+Default.story = {
+	name: 'default',
+};
--- react-chamaje/src/components/Login/Login.tsx
+++ react-chamaje/src/components/Login/Login.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import './Login.css';
+import Background from '../Background/Background';
 
 const Login = () => {
 	return (
-		<div id="wrapper">
-			<div id="navBar">
-				<div id="menuItems">
-					<div>Link</div>
-					<div>Link</div>
-					<div>Link</div>
+		<div>
+			<div id="wrapper">
+				<div id="navBar">
+					<div id="menuItems">
+						<div>Link</div>
+						<div>Link</div>
+						<div>Link</div>
+					</div>
+					<div id="siteTitle">Chamaje</div>
+					<div id="toolBox">
+						<div id="lock">lock</div>
+						<div id="time">17:37</div>
+					</div>
 				</div>
-				<div id="siteTitle">
-					Chamaje
-				</div>
-				<div id="toolBox">
-					<div id="lock">lock</div>
-					<div id="time">time</div>
+				<div id="content">
+					<div id="inputField">type <b>“omer”</b> to login</div>
 				</div>
 			</div>
-			<div id="content">
-				<div id="inputField">typeomer</div>
-			</div>
+			<Background></Background>
 		</div>
 	);
 };
