b99536835e1a35d4681addb6307e29211152b21e
Jee <jeehyun24park@gmail.com>
2023-07-04 13:40:30 +0200

    changed name login to auth

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoginModule } from './login/login.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-	imports: [LoginModule],
+	imports: [AuthModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
