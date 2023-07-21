5f2c290a34e8c84d69b4b7b764f39d257844b8a4
n1kito <mjallada@student.42.fr>
2023-07-21 14:03:51 +0200

    small sizing update

--- react-chamaje/src/components/Friends/Components/FriendBadge/FriendBadge.tsx
+++ react-chamaje/src/components/Friends/Components/FriendBadge/FriendBadge.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from 'react';
 import './FriendBadge.css';
 import jeeAvatar from './images/79132132.jpeg';
-import m3ganAvatar from './images/m3gan.jpg'
+import m3ganAvatar from './images/m3gan.jpg';
 import ShadowWrapper from '../../../Shared/ShadowWrapper/ShadowWrapper';
 
 const FriendBadge = () => {
@@ -9,7 +9,9 @@ const FriendBadge = () => {
 		<ShadowWrapper shadow={true} clickable={true}>
 			<div className="friendBadge" title="Open Jee's profile">
 				<div className="statusIndicator"></div>
-				<div className="badgeAvatar"><img src={m3ganAvatar} /></div>
+				<div className="badgeAvatar">
+					<img src={m3ganAvatar} />
+				</div>
 				<span>@m3gan</span>
 			</div>
 		</ShadowWrapper>
