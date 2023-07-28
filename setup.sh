008fdac017ac7b69e644165223f91439adb26d14
n1kito <mjallada@student.42.fr>
2023-07-28 15:50:09 +0200

    Fixed the script to use exported environment variables if they are available

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Formatting 
+
+GREEN_CHECKMARK="\033[32m✓\033[0m"
 BOLD=$(tput bold)
+DIM="\033[2m"
+UNDERLINE="\033[4m"
 END_C=$(tput sgr0)
 
 # Checking that we are in a .git repo
@@ -9,7 +14,7 @@ if ! git status &> /dev/null; then
     exit 1
 fi
 
-# retrieve the path to the git repo top level
+# Retrieve the path to the git repo top level
 root_dir=$(git rev-parse --show-toplevel)
 
 printf "
@@ -19,22 +24,55 @@ printf "
 
 "
 # retrieve variables from users
-echo "Retrieving database information:"
+echo -e "${DIM}This script will automatically look through your ${BOLD}exported${END_C}${DIM} environment variables and use them if they are available.
+They should have the same name as the following variables.${END_C}"
 echo
-read -p "> ${BOLD}POSTGRES_USER${END_C}:	" POSTGRES_USER
-read -p "> ${BOLD}POSTGRES_PASSWORD${END_C}:	" POSTGRES_PASSWORD
-read -p "> ${BOLD}POSTGRES_DB${END_C}:		" POSTGRES_DB
-echo 
-echo "Retrieving API information"
-echo
-read -p "> ${BOLD}UID${END_C}:			" FT_UID
-read -p "> ${BOLD}SECRET${END_C}:		" FT_SECRET
-echo
-echo "Retrieving JWT secret key"
-echo
-read -p "> ${BOLD}SECRET KEY${END_C}:		" JWT_SECRET_KEY
+echo -e "${UNDERLINE}Retrieving database information${END_C}:"
 echo
 
+if [[ -z "${POSTGRES_USER}" ]]; then
+	read -p "> ${BOLD}POSTGRES_USER${END_C}:	" POSTGRES_USER
+else
+	echo -e "${GREEN_CHECKMARK} ${BOLD}POSTGRES_USER${END_C}:	${POSTGRES_USER}"
+fi
+
+if [[ -z "${POSTGRES_PASSWORD}" ]]; then
+read -p "> ${BOLD}POSTGRES_PASSWORD${END_C}:	" POSTGRES_PASSWORD
+else
+	echo -e "${GREEN_CHECKMARK} ${BOLD}POSTGRES_PASSWORD${END_C}:	${POSTGRES_PASSWORD}"
+fi
+
+if [[ -z "${POSTGRES_DB}" ]]; then
+read -p "> ${BOLD}POSTGRES_DB${END_C}:		" POSTGRES_DB
+else
+	echo -e "${GREEN_CHECKMARK} ${BOLD}POSTGRES_DB${END_C}:		${POSTGRES_DB}"
+fi
+
+echo 
+echo -e "${UNDERLINE}Retrieving API information${END_C}:"
+echo
+if [[ -z "${FT_UID}" ]]; then
+read -p "> ${BOLD}FT_UID${END_C}:			" FT_UID
+else
+	echo -e "${GREEN_CHECKMARK} ${BOLD}FT_UID${END_C}:		${FT_UID}"
+fi
+
+if [[ -z "${FT_SECRET}" ]]; then
+read -p "> ${BOLD}FT_SECRET${END_C}:		" FT_SECRET
+else
+	echo -e "${GREEN_CHECKMARK} ${BOLD}FT_SECRET${END_C}:		${FT_SECRET}"
+fi
+echo
+echo -e "${UNDERLINE}Retrieving JWT secret key${END_C}:"
+echo
+if [[ -z "${JWT_SECRET_KEY}" ]]; then
+read -p "> ${BOLD}SECRET KEY${END_C}:		" JWT_SECRET_KEY
+else
+	echo -e "${GREEN_CHECKMARK} ${BOLD}SECRET KEY${END_C}:		${JWT_SECRET_KEY}"
+fi
+echo
+
+# Create the .env files
 > "$root_dir"/.env printf "\
 # Postgres variables
 POSTGRES_DB=\"$POSTGRES_DB\"
