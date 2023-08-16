43349fbbff9cf9fd93644fe5883b1669343c62f0
n1kito <mjallada@student.42.fr>
2023-08-16 15:51:51 +0200

    removed notification folder

--- nestjs/src/notifications/notifications.gateway.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { NotificationsGateway } from './notifications.gateway';
-
-@Module({
-	controllers: [NotificationsGateway],
-	providers: [NotificationsGateway],
-})
-export class NotificationsGatewayModule {}
