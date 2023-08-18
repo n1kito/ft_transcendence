3e23bcd01af047f14d4dc2ee422a69a1ee6082ea
n1kito <mjallada@student.42.fr>
2023-08-18 15:52:58 +0200

    commented npm run build from react entrypoint script

--- react-chamaje/entrypoint.sh
+++ react-chamaje/entrypoint.sh
@@ -3,10 +3,11 @@
 # Make sure we are using the last version of npm 
 npm install -g npm
 
-# # Make sure we use all last versions of our dependencies
-npm update --save
+# # # Make sure we use all last versions of our dependencies
+# npm update --save
+
 # npm audit fix --force
 
 npm install
-npm run build
+# npm run build
 npm start
